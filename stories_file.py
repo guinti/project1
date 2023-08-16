b52e1a012a6461fb079efaa11a6c1e10809e8107
guinti <112972708+guinti@users.noreply.github.com>
2023-08-16 11:05:27 +0300

    Add files via upload

--- stories_file.py
+++ stories_file.py
@@ -1,32 +1,16 @@
-# import telebot
 from telebot import types
-import sqlite3
 
-from bot_info_file import bot
+import writing_stories
 
-conn = sqlite3.connect('base.bd', check_same_thread=False)
-cur = conn.cursor()
-cur.execute('CREATE TABLE IF NOT EXISTS stories (row_number integer, id integer, page integer(1),'
-            ' story_text varchar(4096), main_theme integer(2), all_pages integer(1))')
-cur.execute('INSERT INTO stories (row_number, id, page, story_text) VALUES(?1, ?2, ?3, ?4)',
-            (1, 1, 1, "первая история"))
-cur.execute('INSERT INTO stories (row_number, id, page, story_text) VALUES(?1, ?2, ?3, ?4)',
-            (2, 1, 2, "первая история продолжение"))
-cur.execute('INSERT INTO stories (row_number, id, page, story_text) VALUES(?1, ?2, ?3, ?4)', (3, 2, 1,
-                                                                                              "Вторая -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------мега длинная история"))
-cur.execute('INSERT INTO stories (row_number, id, page, story_text) VALUES(?1, ?2, ?3, ?4)',
-            (4, 3, 1, "третья история"))
-cur.execute('INSERT INTO stories (row_number, id, page, story_text) VALUES(?1, ?2, ?3, ?4)',
-            (5, 1, 3, "первая история продолжение продолжения"))
-conn.commit()
-cur.close()
+
+from bot_info_file import bot, conn
 
 
 @bot.message_handler(func=lambda message: message.text.lower() == "истории пользователей")
 def stories(message):
     stories_keyboard = types.InlineKeyboardMarkup(row_width=3)
     button_read_stories = types.InlineKeyboardButton('Читать истории', callback_data='read_stories0')
-    button_write_story = types.InlineKeyboardButton('Написать свою', callback_data='write_story')
+    button_write_story = types.InlineKeyboardButton('Мои истории', callback_data='write_story')
     stories_keyboard.add(button_read_stories, button_write_story)
     bot.send_message(message.chat.id, "Здесь вы можете читать и писать истории бла бла бла",
                      # исправить
---
+++ writing_stories.py
@@ -0,0 +1,120 @@
+from telebot import types
+import sqlite3
+
+from bot_info_file import bot, conn
+
+
+@bot.callback_query_handler(func=lambda call: call.data.startswith('write_story'))
+def writing(call):
+    person_stories_keyboard = types.InlineKeyboardMarkup(row_width=3)
+    button_read_my_stories = types.InlineKeyboardButton('Написанные истории', callback_data='read_my_stories1')
+    button_write_story = types.InlineKeyboardButton('Написать свою', callback_data='write_my_story')
+    cur = conn.cursor()
+    cur.execute('SELECT COUNT(*) FROM stories WHERE id=?', (call.from_user.id,))
+    personal_stories_count = cur.fetchone()[0]
+    cur.execute('SELECT * FROM stories WHERE id=?', (call.from_user.id,))
+    if cur.fetchone() is None:
+        person_stories_keyboard.add(button_write_story)
+    elif personal_stories_count == 9:
+        person_stories_keyboard.add(button_read_my_stories)
+    else:
+        person_stories_keyboard.add(button_read_my_stories, button_write_story)
+    bot.edit_message_text("Написать истории бла бла. Правила написания бла бла 4096 бла бла", call.message.chat.id,
+                          call.message.message_id, reply_markup=person_stories_keyboard)
+    cur.close()
+
+
+@bot.callback_query_handler(func=lambda call: call.data == 'write_my_story')
+def writing(call):
+    def last_step(message):
+        story = message.text
+        cur = conn.cursor()
+        cur.execute('SELECT COUNT(*) FROM stories WHERE id=?', (message.chat.id,))
+        page = cur.fetchone()[0]
+        cur.execute('SELECT COUNT(*) FROM stories')
+        row_number = cur.fetchone()[0]
+        cur.execute('INSERT INTO stories(row_number, id, page, story_text) VALUES(?, ?, ?, ?)',
+                    (row_number + 1, message.chat.id, page + 1, story))
+        conn.commit()
+        bot.send_message(message.chat.id, 'Ваша история добавленаВ)')
+        cur.close()
+
+    bot.edit_message_text("Напишите свою сторию. Правила написания бла бла 4096 бла бла кряу", call.message.chat.id,
+                          call.message.message_id)
+    bot.register_next_step_handler(call.message, last_step)
+
+
+@bot.callback_query_handler(func=lambda call: call.data.startswith('read_my_stories'))
+def wrote(call):
+    page = call.data[15]
+    marked = 0
+    if page == 'p':
+        marked = 1
+        page = call.data[16]
+    buttons = []
+    person_stories_keyboard = types.InlineKeyboardMarkup(row_width=3)
+    my_prev_story = types.InlineKeyboardButton('←', callback_data='read_my_storiesp' + str(int(page) - 1))
+    if int(page) > 1:
+        buttons.append(my_prev_story)
+    my_next_story = types.InlineKeyboardButton('→', callback_data='read_my_stories' + str(int(page) + 1))
+    change_story = types.InlineKeyboardButton('Изменить', callback_data='change' + page)
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM stories WHERE id=? AND page=?', (call.from_user.id, int(page)))
+    row = cur.fetchone()
+    buttons.append(change_story)
+    buttons.append(my_next_story)
+    person_stories_keyboard.add(*buttons)
+    if row is None:
+        another_person_stories_keyboard = types.InlineKeyboardMarkup()
+        write_my_story = types.InlineKeyboardButton('Написать историю', callback_data='write_my_story')
+        another_person_stories_keyboard.add(my_prev_story, write_my_story)
+        bot.edit_message_text("Написать историю бла бла. Это будет Ваша " + str(int(page)) + ' история',
+                              call.message.chat.id,
+                              call.message.message_id, reply_markup=another_person_stories_keyboard)
+    else:
+        story_text = row[3]
+        if marked == 1:
+            cur.execute('SELECT * FROM stories WHERE id=? AND page=?', (call.from_user.id, int(page)))
+            story_text = cur.fetchone()[3]
+        bot.edit_message_text(story_text, call.message.chat.id,
+                              call.message.message_id, reply_markup=person_stories_keyboard)
+    cur.close()
+
+
+@bot.callback_query_handler(func=lambda call: call.data.startswith('change'))
+def change(call):
+    page = call.data[6]
+
+    def last_step(message, page):
+        story = message.text
+        cur = conn.cursor()
+        if story == '.':
+            cur.execute('SELECT row_number FROM stories WHERE id=? AND page=?', (message.chat.id, page))
+            row_deleted = cur.fetchone()[0]
+            cur.execute('DELETE FROM stories WHERE id=? AND page=?', (message.chat.id, page))
+            cur.execute('SELECT row_number, page FROM stories WHERE id=? AND page > ? ORDER BY row_number', (message.chat.id, page))
+            rows = cur.fetchall()
+            for row in rows:
+                row_number = row[0]
+                page = row[1] - 1
+                cur.execute('UPDATE stories SET page = ? WHERE row_number = ?', (page, row_number))
+                conn.commit()
+            cur.execute('SELECT COUNT(*) FROM stories',)
+            row_count = cur.fetchone()[0] + 1
+            while (row_deleted <= row_count):
+                row_deleted += 1
+                cur.execute('UPDATE stories SET row_number = ? WHERE row_number = ?', (row_deleted-2, row_deleted - 1))
+                conn.commit()
+            bot.send_message(message.chat.id, 'Ваша история удалена')
+        else:
+            cur.execute('UPDATE stories SET story_text =? WHERE id=? AND page=?', (story, message.chat.id, page))
+            conn.commit()
+            bot.send_message(message.chat.id, 'Ваша история изменена👍')
+
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM stories WHERE id=? AND page=?', (call.from_user.id, page))
+    text = cur.fetchone()[3]
+    bot.edit_message_text(text, call.message.chat.id, call.message.message_id)
+    bot.send_message(call.from_user.id,
+                     "Измените свою историю. Если ваше сообщение будет состоять только из \".\"(точки), ваша история будет удалена. 4096 бла бла кряу")
+    bot.register_next_step_handler(call.message, lambda message: last_step(message, page))
